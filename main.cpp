4f1e456dbfe60abb6b8bdb6b214dd1339aea2eb1
markovojinovic <77206213+markovojinovic@users.noreply.github.com>
2022-06-25 11:49:58 +0200

    novi dan

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 using namespace std;
 
-// Main() function: where the execution of program begins
 int main()
 {
-    // prints hello world
-    cout << "Cao";
+    cout << "Opet da probamo";
 
     return 0;
 }
