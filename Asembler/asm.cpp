e52dccf13bb47d5f6975211981f665a0e593ef85
markovojinovic <77206213+markovojinovic@users.noreply.github.com>
2022-08-23 16:25:11 +0200

    novi

--- Asembler/asm.cpp
+++ Asembler/asm.cpp
@@ -23,17 +23,5 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    // cout << endl;
-    // Asembler as("novi.s", "novi.o");
-    // if (as.start_reading() == 1)
-    // {
-    //     as.print_symbol_table();
-    //     as.exit_protocol();
-    // }
-    // else
-    //     cout << "Zbog greske prevodjenje prekinuto!!!" << endl;
-
     return 0;
 }
-
-// 1:29:56
